aecf9b891220a165581171c205020342f3d82308
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-08-20 22:48:16 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+let pontosEquipe1 = 0;
+let pontosEquipe2 = 0;
+
+function aumentarPontos(equipe, quantidade) {
+  if (equipe === 'equipe1') {
+    pontosEquipe1 += quantidade;
+    document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
+  } else if (equipe === 'equipe2') {
+    pontosEquipe2 += quantidade;
+    document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
+  }
+  if (pontosEquipe1 >= 12) {
+    alert(`${document.getElementById('nomeEquipe1').value || 'Equipe 1'} venceu!`);
+    reiniciarJogo();
+  } else if (pontosEquipe2 >= 12) {
+    alert(`${document.getElementById('nomeEquipe2').value || 'Equipe 2'} venceu!`);
+    reiniciarJogo();
+  }
+}
+
+function reiniciarJogo() {
+  pontosEquipe1 = 0;
+  pontosEquipe2 = 0;
+  document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
+  document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
+}
+
+function zerarPontos1() {
+  pontosEquipe1 = 0;
+  document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
+}
+function zerarPontos2() {
+  pontosEquipe2 = 0;
+  document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
+}
+
+var inputElement = document.getElementById('nomeEquipe1');
+var valorMostradoElement = document.getElementById('nome1');
+
+inputElement.addEventListener('input', function() {
+  var valorInput = inputElement.value;
+  valorMostradoElement.textContent = valorInput;
+});
+
+var inputElement2 = document.getElementById('nomeEquipe2');
+var valorMostradoElement2 = document.getElementById('nome2');
+
+inputElement2.addEventListener('input', function() {
+  var valorInput2 = inputElement2.value;
+  valorMostradoElement2.textContent = valorInput2;
+});
+
